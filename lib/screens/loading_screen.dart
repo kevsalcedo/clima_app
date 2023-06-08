358b1543c1ba3b4e909935597449ed289fea4ae0
KevSalcedo <kevsalcedom@gmail.com>
2023-06-08 00:15:35 -0500

    Add http dependencies

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
