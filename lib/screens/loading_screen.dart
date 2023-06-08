5b0a86502efeddaf8691e9f9441af7bfdd6a7d4c
KevSalcedo <kevsalcedom@gmail.com>
2023-06-08 00:17:07 -0500

    create getData() function.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -17,10 +17,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'),
+    );
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
-    getLocation();
     super.initState();
+    getLocation();
+    getData();
   }
 
   @override
