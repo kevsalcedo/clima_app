307342b54a712eb7d195dad78a4055a1f0925fb5
KevSalcedo <kevsalcedom@gmail.com>
2023-06-08 23:24:49 -0500

    navigate to LocationScreen and pass the var weatherData

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -28,12 +28,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
           'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'),
     );
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(data);
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(
+          locationWeather: weatherData,
+        ),
+      ),
+    );
   }
 
   @override
