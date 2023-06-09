7226e99e77523159dbf571a454cdb4cc8dbfd226
KevSalcedo <kevsalcedom@gmail.com>
2023-06-08 23:25:06 -0500

    remove variables and update url

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -18,14 +18,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     await location.requestPermission();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
 
-  void getData() async {
-    http.Response response = await http.get(
-      Uri.parse(
-          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'),
+    NetworkHelper networkHelper = NetworkHelper(
+      'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric',
     );
 
     var weatherData = await networkHelper.getData();
