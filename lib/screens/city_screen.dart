d382e16e11c51e1061dcab7843338c751784078b
KevSalcedo <kevsalcedom@gmail.com>
2023-06-13 17:05:56 -0500

    create TextField and get the input value in var newCityName.

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -7,6 +7,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  late String newCityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +35,15 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    newCityName = value;
+                  },
+                ),
               ),
               TextButton(
                 onPressed: () {},
